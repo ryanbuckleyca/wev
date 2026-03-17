4da8bd9029c131a9cf420ab7ce4983112e5182b1
Ryan Buckley <ryan.buckley@vidyard.com>
2026-03-17 15:55:28 -0400

    clean-up

--- components/InfoPopover.tsx
+++ components/InfoPopover.tsx
@@ -29,16 +29,16 @@ export default function InfoPopover({ children, content, className = '' }: InfoP
         </div>
       </PopoverTrigger>
       <PopoverContent 
-        className="PopoverContent"
         sideOffset={5}
         collisionPadding={16}
+        className="w-[260px] p-5 text-xs shadow-[0_10px_38px_-10px_hsl(206_22%_7%_/_35%),_0_10px_20px_-15px_hsl(206_22%_7%_/_20%)]"
       >
         {typeof content === 'string' ? (
           <div dangerouslySetInnerHTML={{ __html: content }} />
         ) : (
           content
         )}
-        <PopoverArrow className="PopoverArrow" />
+        <PopoverArrow className="fill-white" />
       </PopoverContent>
     </Popover>
   )
