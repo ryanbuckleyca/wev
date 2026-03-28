a9f3dbb3999846b4fc810594c57aa1a9d3b3aba0
Ryan Buckley <ryan.buckley@vidyard.com>
2026-03-28 06:21:01 -0400

    fix improts

--- components/ui/Popover.tsx
+++ components/ui/Popover.tsx
@@ -3,7 +3,9 @@
 import * as React from "react"
 import * as PopoverPrimitive from "@radix-ui/react-popover"
 
+
 import { cn } from "@/lib/utils"
+import { zIndex } from "@/lib/design-tokens"
 
 const Popover = PopoverPrimitive.Root
 
@@ -22,7 +24,7 @@ const PopoverContent = React.forwardRef<
         "w-72 rounded-md border bg-card p-4 text-popover-foreground popover-shadow outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-[--radix-popover-content-transform-origin]",
         className
       )}
-      style={{ zIndex: require("@/lib/design-tokens").zIndex.popover, ...(props.style || {}) }}
+      style={{ zIndex: zIndex.popover, ...(props.style || {}) }}
       {...props}
     />
   </PopoverPrimitive.Portal>
