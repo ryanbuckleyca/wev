4b5c02126b75066306b76b08f9a9b26f326b47a2
Ryan Buckley <ryan.buckley@vidyard.com>
2026-04-11 16:31:26 -0400

    ignore backup

--- wev-scraper/scripts/check_matches.py
+++ wev-scraper/scripts/check_matches.py
@@ -37,7 +37,7 @@ def main():
     # --- Public client test (simulate browser anon client without session) ---
     try:
         # load frontend env (wev-bulletin) to get PUBLIC URL + key
-        load_dotenv(find_dotenv()))
+        load_dotenv(find_dotenv())
         pub_url = os.environ.get('NEXT_PUBLIC_SUPABASE_URL')
         pub_key = os.environ.get('NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY')
         if pub_url and pub_key:
