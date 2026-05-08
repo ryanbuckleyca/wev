9fa0dde610949c8a4940f902f7363ca4f5a086b5
Ryan Buckley <ryanbuckley@gmail.com>
2026-05-07 21:46:02 -0400

    chore: remove leftover test artifacts

--- test-script.ts
+++
@@ -1,4 +0,0 @@
-import { fetchServerBulletinJobs } from "./wev-bulletin/lib/bulletin/server-data";
-fetchServerBulletinJobs("en")
-  .then((res) => console.log(JSON.stringify(res, null, 2)))
-  .catch((err) => console.error(err));
