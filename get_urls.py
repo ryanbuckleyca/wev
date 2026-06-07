1c255ac90bce87bf341754d619358005157d790a
Ryan Buckley <ryanbuckley@gmail.com>
2026-06-07 18:25:00 -0400

    fix errors

--- get_urls.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.abspath('wev-scraper'))
-from db import get_db
-for s in get_db().table('sources').select('name, url').execute().data:
-    print(f"{s['name']}: {s['url']}")
--- wev-scraper/test_eco.py
+++ wev-scraper/test_eco.py
@@ -1,4 +1,5 @@
 import sys
+
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
