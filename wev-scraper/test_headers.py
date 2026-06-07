9a7091ca6947e674f4669aca050e91349c7fc665
Ryan Buckley <ryanbuckley@gmail.com>
2026-06-07 18:56:51 -0400

    remove unnecessary files

--- wev-scraper/test_headers.py
+++
@@ -1,9 +0,0 @@
-
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True, channel="chrome")
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://httpbin.org/headers")
-    print(page.inner_text("body"))
