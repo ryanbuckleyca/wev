7205294dd825ff1a55049cd9b87c0e4d3b0b6b57
Ryan Buckley <ryanbuckley@gmail.com>
2026-07-23 18:22:45 -0400

    update types

--- wev-bulletin/lib/organizations/types.ts
+++ wev-bulletin/lib/organizations/types.ts
@@ -20,7 +20,7 @@ export interface OrgIndexEntry {
   location: OrgRecord['location'];
   is_sse: OrgRecord['is_sse'];
   type: OrgRecord['type'];
-  sector_id: string | null;
+  sector_id: OrgRecord['sector_id'];
   values_list: OrgRecord['values_list'];
   // RPC-computed fields
   active_job_count: number;
