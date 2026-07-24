6e371ad236faee3d66e05cb52f1d464fd923d088
Ryan Buckley <ryanbuckley@gmail.com>
2026-07-24 16:04:18 -0400

    next-intl fallback

--- wev-bulletin/components/OrganizationProfileHeader.tsx
+++ wev-bulletin/components/OrganizationProfileHeader.tsx
@@ -77,10 +77,7 @@ export default function OrganizationProfileHeader({
 
             {sectorLabel && (
               <div className="flex items-center gap-1.5">
-                <span className="font-medium text-foreground">
-                  {t('sector')}:
-                </span>{' '}
-                {sectorLabel}
+                <span className="font-medium text-foreground">{t('sector')}:</span> {sectorLabel}
               </div>
             )}
           </div>
