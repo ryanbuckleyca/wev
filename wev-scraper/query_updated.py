6d05f840d8e941bc578cccceb65921160411899d
Ryan Buckley <ryanbuckley@gmail.com>
2026-08-24 22:00:00 -0400

    restore some tests

--- wev-scraper/query_updated.py
+++
@@ -1,9 +0,0 @@
-import json
-from utils.db import supabase
-from datetime import datetime, timezone, timedelta
-
-try:
-    res = supabase.table('jobs').select('id').limit(1).execute()
-    print("Schema test successful. Does created_at exist?")
-except Exception as e:
-    print(e)
