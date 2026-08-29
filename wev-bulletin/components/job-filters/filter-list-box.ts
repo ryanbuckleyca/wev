32333122da0c781dbf29094fa1321e02fc6aa1cd
Ryan Buckley <ryanbuckley@gmail.com>
2026-08-29 11:06:15 -0400

    tweak height of filter options

--- wev-bulletin/components/job-filters/filter-list-box.ts
+++ wev-bulletin/components/job-filters/filter-list-box.ts
@@ -1,3 +1,3 @@
 /** Shared list box so org (and matching job) filter sections align in the grid. */
 export const FILTER_LIST_BOX_CLASS =
-  'h-48 overflow-y-auto border border-border rounded-wev-btn p-2 bg-background';
+  'h-40 overflow-y-auto border border-border rounded-wev-btn p-2 bg-background';
